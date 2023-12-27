68b16c639b51927f2971015ca3bef32e25fe9f14
Cukierr <117932633+Cukierr@users.noreply.github.com>
2023-12-27 20:02:20 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -156,7 +156,7 @@ window.addEventListener("load", function(){
             else if(l == 1){
                 d.parentElement.style.border = '2px solid whitesmoke';
                 d.parentElement.addEventListener('mouseenter', function(){
-                    console.log(screem.width)
+                    console.log(window.width)
                     if(screem.width>1024){
                         d.parentElement.style.border = '2px solid #04b604';
                     }
